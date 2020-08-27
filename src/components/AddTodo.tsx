9dd878c1318a62827c6b5872318f42ed42b54f0a
fernancumez <fernancumez@gmail.com>
2020-08-27 17:36:10 -0600

    Add Component Todo Form

---
+++ src/components/AddTodo.tsx
@@ -0,0 +1,34 @@
+import React, { FC, FormEvent, useState } from "react";
+
+type Props = {
+  saveTodo: (evt: FormEvent, formData: ITodo | any) => void;
+};
+
+const AddTodo: FC<Props> = ({ saveTodo }) => {
+  const [formData, setFormData] = useState<ITodo | {}>();
+
+  const handleForm = (evt: FormEvent<HTMLInputElement>): void => {
+    setFormData({
+      ...formData,
+      [evt.currentTarget.id]: evt.currentTarget.value,
+    });
+  };
+
+  return (
+    <form className="Form" onSubmit={(evt) => saveTodo(evt, formData)}>
+      <div>
+        <div>
+          <label htmlFor="name">Name</label>
+          <input onChange={handleForm} type="text" id="name" />
+        </div>
+        <div>
+          <label htmlFor="description">Description</label>
+          <input onChange={handleForm} type="text" id="description" />
+        </div>
+      </div>
+      <button disabled={formData === undefined ? true : false}>Add Todo</button>
+    </form>
+  );
+};
+
+export default AddTodo;
